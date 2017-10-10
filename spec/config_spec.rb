9f82ba71854391c1287d424c24de238db14c1ff3
Charles Bushong <bushong1@gmail.com>
2017-10-10 08:35:37 -0400

    Updating notification message; updating spec

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
-describe 'config/alphagov.yml' do
-  it 'is valid YAML' do
-    config = YAML.load_file('config/alphagov.yml')
+config_files = Dir.glob('config/*.yml').select{ |e| File.file? e }
 
-    expect(config).to be_a(Hash)
+config_files.each do |file|
+  describe file do
+    it 'is valid YAML' do
+      config = YAML.load_file(file)
+      expect(config).to be_a(Hash)
+    end
   end
 end
